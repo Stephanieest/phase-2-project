cd7b8cdb2152ebbd28e6293b787a3961ce50c936
Stephanie <stephnjonjo@gmail.com>
2024-11-13 12:28:22 +0300

    creates all the components and Writes code for the Landing page in home.js and imports home.js in App.js

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>
+                Welcome to Steph-FC!! -  Where Every Meal is Made with Love
+            </h1>
+            <p>
+                Step into the warmth of Steph-FC!!!! A cozy haven where every dish is crafted with care and passion. From savory, soul-satisfying meals to decadent desserts that make every bite unforgettable, we’ve got something for everyone. Whether you’re here for a casual meal with friends or a special celebration, our menu features fresh ingredients and flavors that will make you feel right at home. Come hungry, leave happy, and let us take care of the rest. 
+            </p>
+        </div>
+    )
+}
