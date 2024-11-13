6cf919d263392c3e6937db9baf9abd1d615b8d97
Stephanie <stephnjonjo@gmail.com>
2024-11-13 10:28:14 +0300

    creates a db.json and a Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Cart</li>
+        <li>Favourites</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
