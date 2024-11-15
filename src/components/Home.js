393aa16a08fd6dd729f7d1946405848c7da733eb
Stephanie <stephnjonjo@gmail.com>
2024-11-15 15:51:48 +0300

    adds emojis to home and navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,10 +5,10 @@ const Home = () => {
     return (
         <div className="home">
             <h1>
-                Welcome to Steph-FC!! -  Where Every Meal is Made with Love ❤️
+                Welcome to Steph-FC!! -  Where Every Meal is Made with Love 😋❤️
             </h1>
             <p>
-                Step into the warmth of Steph-FC!!!! A cozy haven where every dish is crafted with care and passion. From savory, soul-satisfying meals to decadent desserts that make every bite unforgettable, we’ve got something for everyone. Whether you’re here for a casual meal with friends or a special celebration, our menu features fresh ingredients and flavors that will make you feel right at home. Come hungry, leave happy, and let us take care of the rest😊. 
+                Step into the warmth of Steph-FC!!!!🏬 A cozy haven where every dish is crafted with care and passion👩🏽‍🍳🍳🍽️. From savory, soul-satisfying meals to decadent desserts that make every bite unforgettable, we’ve got something for everyone. Whether you’re here for a casual meal with friends or a special celebration, our menu features fresh ingredients and flavors that will make you feel right at home🏠. Come hungry🥺, leave happy😁😃, and let us take care of the rest😊. 
             </p>
 
             <img
